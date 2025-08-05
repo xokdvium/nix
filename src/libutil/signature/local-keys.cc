e86a24c090b67302e66ced381c1cb2f5eb5d009d
John Ericson <John.Ericson@Obsidian.Systems>
2025-08-05 14:12:00 -0400

    Clean up Base* code
    Make it separate from Hash, since other things can be base-encoded too.
    
    This isn't really needed for Nix, but it makes the code easier to read
    e.g. for someone reimplementing this stuff in a different language. (Of
    course, Base16/Base64 should be gotten off-the-shelf, but now the hash
    code, which is more bespoke, is less cluttered with the parts that would
    be from some library.)
    
    Many reimplementations of "Nix32" and our hash type already exist, so
    this cleanup is coming years too late, but I say better late than never
    / it is always good to nudge the code in the direction of being a
    "living spec".
    
    Co-authored-by: Sergei Zimmerman <sergei@zimmerman.foo>

--- src/libutil/signature/local-keys.cc
+++ src/libutil/signature/local-keys.cc
@@ -1,6 +1,7 @@
 #include "nix/util/signature/local-keys.hh"
 
 #include "nix/util/file-system.hh"
+#include "nix/util/base-n.hh"
 #include "nix/util/util.hh"
 #include <sodium.h>
 
@@ -25,7 +26,7 @@ Key::Key(std::string_view s, bool sensitiveValue)
         if (name == "" || key == "")
             throw FormatError("key is corrupt");
 
-        key = base64Decode(key);
+        key = base64::decode(key);
     } catch (Error & e) {
         std::string extra;
         if (!sensitiveValue)
@@ -37,7 +38,7 @@ Key::Key(std::string_view s, bool sensitiveValue)
 
 std::string Key::to_string() const
 {
-    return name + ":" + base64Encode(key);
+    return name + ":" + base64::encode(std::as_bytes(std::span<const char>{key}));
 }
 
 SecretKey::SecretKey(std::string_view s)
@@ -49,23 +50,23 @@ SecretKey::SecretKey(std::string_view s)
 
 std::string SecretKey::signDetached(std::string_view data) const
 {
-    unsigned char sig[crypto_sign_BYTES];
+    uint8_t sig[crypto_sign_BYTES];
     unsigned long long sigLen;
-    crypto_sign_detached(sig, &sigLen, (unsigned char *) data.data(), data.size(), (unsigned char *) key.data());
-    return name + ":" + base64Encode(std::string((char *) sig, sigLen));
+    crypto_sign_detached(sig, &sigLen, (uint8_t *) data.data(), data.size(), (uint8_t *) key.data());
+    return name + ":" + base64::encode(std::as_bytes(std::span<const uint8_t>{sig, sigLen}));
 }
 
 PublicKey SecretKey::toPublicKey() const
 {
-    unsigned char pk[crypto_sign_PUBLICKEYBYTES];
-    crypto_sign_ed25519_sk_to_pk(pk, (unsigned char *) key.data());
+    uint8_t pk[crypto_sign_PUBLICKEYBYTES];
+    crypto_sign_ed25519_sk_to_pk(pk, (uint8_t *) key.data());
     return PublicKey(name, std::string((char *) pk, crypto_sign_PUBLICKEYBYTES));
 }
 
 SecretKey SecretKey::generate(std::string_view name)
 {
-    unsigned char pk[crypto_sign_PUBLICKEYBYTES];
-    unsigned char sk[crypto_sign_SECRETKEYBYTES];
+    uint8_t pk[crypto_sign_PUBLICKEYBYTES];
+    uint8_t sk[crypto_sign_SECRETKEYBYTES];
     if (crypto_sign_keypair(pk, sk) != 0)
         throw Error("key generation failed");
 
@@ -93,7 +94,7 @@ bool PublicKey::verifyDetachedAnon(std::string_view data, std::string_view sig) 
 {
     std::string sig2;
     try {
-        sig2 = base64Decode(sig);
+        sig2 = base64::decode(sig);
     } catch (Error & e) {
         e.addTrace({}, "while decoding signature '%s'", sig);
     }
@@ -101,7 +102,7 @@ bool PublicKey::verifyDetachedAnon(std::string_view data, std::string_view sig) 
         throw Error("signature is not valid");
 
     return crypto_sign_verify_detached(
-               (unsigned char *) sig2.data(), (unsigned char *) data.data(), data.size(), (unsigned char *) key.data())
+               (uint8_t *) sig2.data(), (uint8_t *) data.data(), data.size(), (uint8_t *) key.data())
            == 0;
 }
 
